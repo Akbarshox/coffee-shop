93247ab3d9818dc14743201ff99867f033d8596f
Akbarshox <akbarshax@bk.ru>
2020-10-10 17:48:08 +0500

    sidebar-over

--- src/components/Appbar/SideDrawer.js
+++ src/components/Appbar/SideDrawer.js
@@ -6,6 +6,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import YandexMap from "../Map/Map";
 import style from './Appbar.module.css';
+import SignIn from "../Auth/SignIn";
+import user from '../../assets/images/user.svg';
+import home from '../../assets/images/home.svg';
+import cart from '../../assets/images/cart.svg';
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
    list: {
@@ -33,7 +38,7 @@ export default function SideDrawer() {
 
    const sideList = side => (
       <div
-         className={classes.list}
+         className={[classes.list, style.list].join(' ')}
          role="presentation"
          // onClick={toggleDrawer(side, false)}
          // onKeyDown={toggleDrawer(side, false)}
@@ -41,6 +46,10 @@ export default function SideDrawer() {
          <div className={style.sidedrawerList}>
             <YandexMap/>
          </div>
+         <div className={style.user}><img src={user} alt="user"/><SignIn/></div>
+         <div className={style.user}><img src={home} alt="home"/><Link to="/">Главная</Link></div>
+         <div className={style.user}><img src={cart} alt="cart"/><Link to="/">Корзина</Link></div>
+         <h3>+998 77 555 00 00</h3>
       </div>
    );
 
