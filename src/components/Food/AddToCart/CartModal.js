654daef2979862700518719dd5bb305bc9659e3e
Akbarshox <akbarshax@bk.ru>
2021-03-15 18:01:13 +0500

    photo edit

--- src/components/Food/AddToCart/CartModal.js
+++ src/components/Food/AddToCart/CartModal.js
@@ -3,20 +3,21 @@ import TransitionsModal from "../../UI/Modal";
 import Button from "../../UI/Button";
 import style from './modal.module.css';
 import {Store} from "../../../Store";
+import cancel from '../../../assets/images/cancel.svg';
 
 export default function CartModal(props) {
    const [open, setOpen] = React.useState(false);
-   const {dispatch} = useContext(Store);
+   const {state, dispatch} = useContext(Store);
 
    const handleOpen = (e) => {
       setOpen(true);
-      console.log(props)
-      // return dispatch({type: 'ADD-TO-CART', payload: props})
+      return dispatch({type: 'ADD-TO-CART', payload: props})
    }
    const handleClose = () => {
       setOpen(false);
+      // return dispatch({type: 'ADD-TO-CART', payload: []})
    };
-
+   console.log(state.addToCart)
    return (
       <div>
          <div className={style.button}>
@@ -24,13 +25,14 @@ export default function CartModal(props) {
          </div>
          <TransitionsModal open={open} handleClose={handleClose}>
             <div className={style.modal}>
-               <img src={props.image} alt="image"/>
+               <img src={props.image} alt="image" className={style.modalPic}/>
+               <img src={cancel} alt="" className={style.cancel} onClick={handleClose}/>
                <h2>{props.name}</h2>
                <h4>Количество</h4>
                <div className={style.price}>
                   <p>1</p>
                   <p>{props.price}</p>
-                  <span><button>-</button><button>+</button></span>
+                  <span><Button btnType="PlusMinus">-</Button><Button btnType="PlusMinus">+</Button></span>
                </div>
             </div>
          </TransitionsModal>
