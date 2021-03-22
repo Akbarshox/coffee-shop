6830e30d429ebeb21437d3e3ac9060cbb4892739
Akbarshox <akbarshax@bk.ru>
2021-03-22 22:55:10 +0500

    AddToCart

--- src/components/Food/AddToCart/CartModal.js
+++ src/components/Food/AddToCart/CartModal.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import TransitionsModal from "../../UI/Modal";
 import Button from "../../UI/Button";
 import style from './modal.module.css';
@@ -6,18 +6,39 @@ import {Store} from "../../../Store";
 import cancel from '../../../assets/images/cancel.svg';
 
 export default function CartModal(props) {
-   const [open, setOpen] = React.useState(false);
    const {state, dispatch} = useContext(Store);
+   const [open, setOpen] = useState(false);
+   const [cart, setCart] = useState([]);
 
    const handleOpen = (e) => {
       setOpen(true);
-      return dispatch({type: 'ADD-TO-CART', payload: props})
+      // return dispatch({type: 'ADD-TO-CART', payload: props})
    }
    const handleClose = () => {
       setOpen(false);
       // return dispatch({type: 'ADD-TO-CART', payload: []})
    };
-   console.log(state.addToCart)
+
+   const handleAddToCart = (e) => {
+      if (cart.length !== 0) {
+         setCart(
+            cart.map(v =>
+               v.name === e.name && v.price === e.price ?
+                  {...v, ["count"]: v.count + 1} : v
+            )
+         )
+      } else
+         setCart([...cart, {"name": e.name, "price": e.price, "image": e.image, "count": 1}])
+   }
+   const handleDeleteFromCart = () => {
+
+   }
+   const handleApproveOrder = () => {
+
+   }
+
+   console.log(cart)
+
    return (
       <div>
          <div className={style.button}>
@@ -32,8 +53,12 @@ export default function CartModal(props) {
                <div className={style.price}>
                   <p>1</p>
                   <p>{props.price}</p>
-                  <span><Button btnType="PlusMinus">-</Button><Button btnType="PlusMinus">+</Button></span>
+                  <span>
+                     <Button btnType="PlusMinus" clicked={() => handleDeleteFromCart(props)}>-</Button>
+                     <Button btnType="PlusMinus" clicked={() => handleAddToCart(props)}>+</Button>
+                  </span>
                </div>
+               <Button btnType="auth" clicked={() => handleApproveOrder()}>Подтвердить</Button>
             </div>
          </TransitionsModal>
       </div>
